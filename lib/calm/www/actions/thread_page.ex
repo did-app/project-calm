23d7b8dc62ae76fded06bc70dc399caeaeb60402
Peter Saxton <peterhsaxton@gmail.com>
2020-04-16 16:18:14 +0100

    use update content in template

--- lib/calm/www/actions/thread_page.ex
+++ lib/calm/www/actions/thread_page.ex
@@ -1,9 +1,56 @@
 defmodule Calm.WWW.Actions.ThreadPage do
   use Raxx.SimpleServer
-  use Calm.WWW.Layout, arguments: [:csrf_token]
+  use Calm.WWW.Layout, arguments: [:updates, :csrf_token]
 
   def handle_request(_request, _config) do
+    updates = [
+      %{
+        text:
+          ~s"""
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+          https://did.app/docs
+          A much smaller random paragraph
+          """
+          |> String.trim(),
+        name: "Peter",
+        color: "teal",
+        time: "Yesterday"
+      },
+      %{
+        text:
+          ~s"""
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          """
+          |> String.trim(),
+        name: "Susan Furtado",
+        color: "gray",
+        time: "2 hours ago"
+      },
+      %{
+        text:
+          ~s"""
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+          - one
+          - two
+
+          1. one
+          2. two
+
+          A much smaller random paragraph
+          """
+          |> String.trim(),
+        name: "Peter",
+        color: "teal",
+        time: "5 minutes ago"
+      }
+    ]
+
     response(:ok)
-    |> render("dummy")
+    |> render(updates, "dummy")
+
+    # TODO post anywhere
+    # Create a link
   end
 end
